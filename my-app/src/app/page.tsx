424646b106d67cb2dffa7a36e24a4165ac60206f
Md. Saddam Hossain Saykat <90493307+Saddamsaykat@users.noreply.github.com>
2025-11-15 01:02:34 +0600

    update

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline h-screen flex items-center justify-center">
+        Hello VPS
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
